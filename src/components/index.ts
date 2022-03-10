e70bd30adf7c60560d5e1509ae05ec344cd7d411
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-03-10 13:32:56 +0500

    exported new components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from "./Button";
+export { default as Header } from "./Header";
+export { default as Layout } from "./Layout";
+
+// Pages
+export { HomePage } from "./Pages";
