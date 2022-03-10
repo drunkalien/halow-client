ba43d2635c0b27046645c99fbb34ee5cca0e9651
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-03-10 13:32:40 +0500

    pages

---
+++ src/components/Pages/Home/Home.tsx
@@ -0,0 +1,17 @@
+import cn from "classnames";
+
+import { Button } from "../..";
+import classes from "./home.module.scss";
+
+const Home = () => {
+  return (
+    <div className={cn(classes.home)}>
+      <div className={cn(classes.buttons)}>
+        <Button>Join meeting</Button>
+        <Button type="secondary">Create meeting</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
