9bc628931fdcd6d95136368a2e43dcf599fcde82
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-03-11 18:22:04 +0500

    some import exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Button } from "./Button";
 export { default as Header } from "./Header";
 export { default as Layout } from "./Layout";
+export { default as Input } from "./Input";
 
 // Pages
-export { HomePage } from "./Pages";
+export { HomePage, SignupPage } from "./Pages";
