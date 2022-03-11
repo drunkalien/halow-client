1095f6448832650bf758963d11c28a26143d4560
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-03-11 18:21:44 +0500

    imported logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import cn from "classnames";
 
+import { Logo } from "../Icons";
 import classes from "./head.module.scss";
 
 const Header = () => {
   return (
     <header className={cn(classes.header)}>
-      <p>Halow</p>
+      <Logo />
     </header>
   );
 };
