956c3064b2562b0bf51671bcf79245e86130def5
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-03-14 15:45:29 +0500

    added routing

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -1,13 +1,19 @@
 import cn from "classnames";
+import { useNavigate } from "react-router-dom";
 
 import { Button } from "../..";
 import classes from "./home.module.scss";
 
 const Home = () => {
+  const history = useNavigate();
+  function handleJoinMeeting() {
+    history("/join");
+  }
+
   return (
     <div className={cn(classes.home)}>
       <div className={cn(classes.buttons)}>
-        <Button>Join meeting</Button>
+        <Button onClick={handleJoinMeeting}>Join meeting</Button>
         <Button variant="secondary">Create meeting</Button>
       </div>
     </div>
