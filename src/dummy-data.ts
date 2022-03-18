34e2ccf4a183443504238ccf807cdaa10e345ecd
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-03-18 10:45:52 +0500

    added dummy data

---
+++ src/dummy-data.ts
@@ -0,0 +1,153 @@
+type PeerType = {
+  username: string;
+  firstName: string;
+  lastName: string;
+  isHost: boolean;
+};
+
+export const data: PeerType[] = [
+  {
+    username: "khuja",
+    firstName: "Jamshidkhuja",
+    lastName: "Burikhujaev",
+    isHost: true,
+  },
+  {
+    username: "Zieu",
+    firstName: "Ziyodulla",
+    lastName: "Muydinjanov",
+    isHost: false,
+  },
+  {
+    username: "j_doe",
+    firstName: "John",
+    lastName: "Doe",
+    isHost: false,
+  },
+  {
+    username: "sleepy",
+    firstName: "Doe",
+    lastName: "John",
+    isHost: false,
+  },
+  {
+    username: "khuja",
+    firstName: "Jamshidkhuja",
+    lastName: "Burikhujaev",
+    isHost: true,
+  },
+  {
+    username: "Zieu",
+    firstName: "Ziyodulla",
+    lastName: "Muydinjanov",
+    isHost: false,
+  },
+  {
+    username: "j_doe",
+    firstName: "John",
+    lastName: "Doe",
+    isHost: false,
+  },
+  {
+    username: "sleepy",
+    firstName: "Doe",
+    lastName: "John",
+    isHost: false,
+  },
+  {
+    username: "khuja",
+    firstName: "Jamshidkhuja",
+    lastName: "Burikhujaev",
+    isHost: true,
+  },
+  {
+    username: "Zieu",
+    firstName: "Ziyodulla",
+    lastName: "Muydinjanov",
+    isHost: false,
+  },
+  {
+    username: "j_doe",
+    firstName: "John",
+    lastName: "Doe",
+    isHost: false,
+  },
+  {
+    username: "sleepy",
+    firstName: "Doe",
+    lastName: "John",
+    isHost: false,
+  },
+  {
+    username: "khuja",
+    firstName: "Jamshidkhuja",
+    lastName: "Burikhujaev",
+    isHost: true,
+  },
+  {
+    username: "Zieu",
+    firstName: "Ziyodulla",
+    lastName: "Muydinjanov",
+    isHost: false,
+  },
+  {
+    username: "j_doe",
+    firstName: "John",
+    lastName: "Doe",
+    isHost: false,
+  },
+  {
+    username: "sleepy",
+    firstName: "Doe",
+    lastName: "John",
+    isHost: false,
+  },
+  {
+    username: "khuja",
+    firstName: "Jamshidkhuja",
+    lastName: "Burikhujaev",
+    isHost: true,
+  },
+  {
+    username: "Zieu",
+    firstName: "Ziyodulla",
+    lastName: "Muydinjanov",
+    isHost: false,
+  },
+  {
+    username: "j_doe",
+    firstName: "John",
+    lastName: "Doe",
+    isHost: false,
+  },
+  {
+    username: "sleepy",
+    firstName: "Doe",
+    lastName: "John",
+    isHost: false,
+  },
+  {
+    username: "khuja",
+    firstName: "Jamshidkhuja",
+    lastName: "Burikhujaev",
+    isHost: true,
+  },
+  {
+    username: "Zieu",
+    firstName: "Ziyodulla",
+    lastName: "Muydinjanov",
+    isHost: false,
+  },
+  {
+    username: "j_doe",
+    firstName: "John",
+    lastName: "Doe",
+    isHost: false,
+  },
+  {
+    username: "sleepy",
+    firstName: "Doe",
+    lastName: "John",
+    isHost: false,
+  },
+];
