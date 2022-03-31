07fe2c260787cb2790a490f3e0f55ea2b12c39ab
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-03-31 10:04:28 +0500

    started adding webrtc and sockets

--- src/components/Pages/Room/Room.tsx
+++ src/components/Pages/Room/Room.tsx
@@ -1,11 +1,90 @@
+import { useState, useRef, useEffect } from "react";
+import { io } from "socket.io-client";
 import cn from "classnames";
+import Peer from "simple-peer";
 
 import classes from "./room.module.scss";
 import { data } from "../../../dummy-data";
 
 import { Container, Button } from "../..";
 
+type PeerType = {
+  peerId: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+};
+
 const Room = () => {
+  const [peers, setPeers] = useState<any[]>([]);
+  const socketRef: any = useRef();
+  const userAudio: any = useRef();
+  const peersRef: any = useRef([]);
+
+  useEffect(() => {
+    socketRef.current = io("http://localhost:5000");
+    navigator.mediaDevices
+      .getUserMedia({ video: false, audio: true })
+      .then((stream) => {
+        userAudio.current = stream;
+        socketRef.current.emit("get-all-users", 100000);
+        socketRef.current.on("get-all-users", (users: PeerType[]) => {
+          const peers: Peer.Instance[] = [];
+          users.forEach(({ peerId }: PeerType) => {
+            const peer = createPeer(peerId, socketRef.current.id, stream);
+
+            peersRef.current.push({
+              peerId: peerId,
+              peer,
+            });
+            peers.push(peer);
+          });
+          setPeers(peers);
+        });
+      });
+  }, []);
+
+  function createPeer(
+    userToSIgnal: string,
+    callerId: string,
+    stream: MediaStream
+  ) {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream,
+    });
+
+    peer.on("signal", (signal: any) => {
+      socketRef.current.emit("sending-signal", {
+        userToSIgnal,
+        callerId,
+        signal,
+      });
+    });
+
+    return peer;
+  }
+
+  function addPeer(
+    incomingSiganl: string,
+    callerId: string,
+    stream: MediaStream
+  ) {
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream,
+    });
+
+    peer.on("signal", (signal: any) => {
+      socketRef.current.emit("returning-signal", { signal, callerId });
+    });
+
+    peer.signal(incomingSiganl);
+    return peer;
+  }
+
   return (
     <Container>
       <div className={cn(classes["container"])}>
