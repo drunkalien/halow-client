081f3ae5e0ad12da9d0d00a8a2cf9095fbc0c91b
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-03-31 10:05:07 +0500

    added username

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,21 @@
 import cn from "classnames";
+import { useNavigate } from "react-router-dom";
 import { Container } from "../";
 
 import { Logo } from "../Icons";
 import classes from "./head.module.scss";
 
 const Header = () => {
+  const history = useNavigate();
   return (
     <header className={cn(classes.header)}>
       <Container>
-        <Logo />
+        <div className={cn(classes.container)}>
+          <Logo />
+          <p className={cn(classes.profile)} onClick={() => history("profile")}>
+            username
+          </p>
+        </div>
       </Container>
     </header>
   );
