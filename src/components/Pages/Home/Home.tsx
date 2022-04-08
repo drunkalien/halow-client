1629e99ffc3b85bc024518f2d0b5433d73f3947c
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-04-08 15:08:24 +0500

    added socket connection

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -2,16 +2,35 @@ import cn from "classnames";
 import { Link } from "react-router-dom";
 
 import { Button } from "../..";
+import { useWebsocket } from "../../../context";
 import classes from "./home.module.scss";
 
 const Home = () => {
+  const socket = useWebsocket();
+  socket?.on("create-room", (room: any) => {
+    console.log(room);
+  });
+
   return (
     <div className={cn(classes.home)}>
       <div className={cn(classes.buttons)}>
         <Link to="/join">
           <Button>Join meeting</Button>
         </Link>
-        <Button variant="secondary">Create meeting</Button>
+        <Button
+          variant="secondary"
+          onClick={() => {
+            console.log("a");
+            socket?.emit("create-room", {
+              peerId: "peerid",
+              username: "zieu",
+              firstName: "firstName",
+              lastName: "lastName",
+            });
+          }}
+        >
+          Create meeting
+        </Button>
       </div>
     </div>
   );
