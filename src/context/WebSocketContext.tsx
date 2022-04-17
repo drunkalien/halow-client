75c6ee9bb04af47a9813ce8a7deff6c72ca5d25f
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-04-17 20:02:51 +0500

    changed socket url

--- src/context/WebSocketContext.tsx
+++ src/context/WebSocketContext.tsx
@@ -24,7 +24,7 @@ const WebsocketProvider: FunctionComponent<{ children: ReactNode }> = ({
   useEffect(() => {
     try {
       const socketConnection = io(
-        "https://halow-server.herokuapp.com/",
+        "ws://halow-server.herokuapp.com/",
         options
       ).connect();
       setConnection(socketConnection);
