c69bc02feddcd58f8692ca0b9cf19ba858f3f493
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-05-12 16:57:23 +0500

    some changes

--- src/context/WebSocketContext.tsx
+++ src/context/WebSocketContext.tsx
@@ -26,7 +26,7 @@ const WebsocketProvider: FunctionComponent<{ children: ReactNode }> = ({
       const socketConnection = io(
         // "https://halow-server.herokuapp.com:5001/",
         // "http://localhost:5000",
-        " https://7405-195-158-4-4.eu.ngrok.io/",
+        "https://7405-195-158-4-4.eu.ngrok.io/",
         options
       ).connect();
       setConnection(socketConnection);
